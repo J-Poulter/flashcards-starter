5efb1de450db712468fd471c6b94e072c78aa8b7
Jeremy Poulter <Jpoulter@gmail.com>
2020-01-30 22:27:44 -0700

    add current round property to Game class, define start method, and link require files

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,24 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Deck = require('./Deck');
+const Round = require('./Round');
 
 class Game {
-  constructor() {}
-
+  constructor() {
+  this.currentRound = 0;
+}
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
+  }
+
+  start() {
+    const deck = new Deck(prototypeQuestions);
+    const round = new Round(deck);
+    this.printMessage(deck, round);
+    this.printQuestion(round);
+
   }
 
   printQuestion(round) {
